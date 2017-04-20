73faf7dbe3f6026893f25265659930504f0bc6a3
ablears <hello@webdesignpeople.net>
2017-04-21 11:54:18 +1200

    Add form-row-first class to billing_email

--- classes/class-woocommerce-email-validation.php
+++ classes/class-woocommerce-email-validation.php
@@ -37,6 +37,7 @@ class WooCommerce_Email_Validation {
 			$return_fields[ $field_key ] = $field_data;
 
 			if( 'billing_email' == $field_key ) {
+				$return_fields['billing_email']['class']=apply_filters( 'woocommerce_confirm_email_field_class', array( 'form-row-first' ) );
 				$return_fields['billing_email-2'] = array(
 					'label' 			=> __( 'Confirm Email Address', 'woocommerce-email-validation' ),
 					'placeholder' 		=> _x( 'Email Address', 'placeholder', 'woocommerce-email-validation' ),
